85e7ad4cf081a1473da48c1f19d725789c8ef362
ziad1bohlok <ziad.m.bohlok99@gmail.com>
2024-07-15 15:08:43 +0300

    update database2

--- src/main/java/com/example/advanceRestApi/models/User.java
+++ src/main/java/com/example/advanceRestApi/models/User.java
@@ -8,7 +8,7 @@ import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
-@Entity
+@Entity(name = "user_details")
 public class User {
    @Id
    @GeneratedValue
---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into user_details(id,name,birthday)
+values(1001,'ziad',7-15-2024);
